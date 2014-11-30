9ef446cafdbebeceb160acc48c5088dbd15b17fe
gnodar01 <gnodar01@gmail.com>
2014-11-29 22:52:54 -0500

    Add randomized speed function to enemies.

--- js/app.js
+++ js/app.js
@@ -14,6 +14,7 @@ var Enemy = function() {
     this.x = -100;
     this.randomize = Math.floor(Math.random()*3);
     this.y = enemyRow[this.randomize];
+    this.speed = Math.floor(Math.random()*5);
 
 }
 
@@ -23,7 +24,7 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
-    this.x += 100 * dt;
+    this.x += 50 * this.speed * dt;
     if (this.x > 503) {
         this.x = -100;
     }
