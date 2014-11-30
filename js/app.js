f396995eb4e8a149fa83cb76ac753315cf9b88b1
gnodar01 <gnodar01@gmail.com>
2014-11-29 22:48:01 -0500

    Add X and Y boundry limits to prevent player from going off screen.

--- js/app.js
+++ js/app.js
@@ -58,16 +58,16 @@ Player.prototype.render = function() {
 }
 
 Player.prototype.handleInput = function(direction) {
-    if (direction === 'left') {
+    if (direction === 'left' && this.x > 0) {
         this.x -= 100;
     }
-    else if (direction === 'right') {
+    else if (direction === 'right' && this.x < 400) {
         this.x += 100;
     }
-    else if (direction === 'up') {
+    else if (direction === 'up' && this.y > 100) {
         this.y -= 85;
     }
-    else if (direction === 'down') {
+    else if (direction === 'down' && this.y < 350) {
         this.y += 85;
     }
 }
@@ -96,6 +96,7 @@ document.addEventListener('keyup', function(e) {
     player.handleInput(allowedKeys[e.keyCode]);
 });
 
+//Prevents window scrolling using up/down keys.
 window.addEventListener("keydown", function(e) {
     // space and arrow keys
     if([32, 37, 38, 39, 40].indexOf(e.keyCode) > -1) {
